b949c62a04437bd777fb2892e6f0521211fd49ee
hyper linked <45010713+kyledkearney@users.noreply.github.com>
2019-05-12 22:21:13 -0600

    updated nav-bar with links

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -4,6 +4,14 @@ function Header() {
 return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <h1 className="navbar-brand">Bookworm</h1>
+        <ul className='navbar-nav'>
+            <li className='nav-item'>
+                <a class='nav-link' href='/'>Search</a>
+            </li>
+            <li className='nav-item'>
+                <a class='nav-link' href='/saved'>Saved</a>
+            </li>
+        </ul>   
     </nav> 
 )
 }
