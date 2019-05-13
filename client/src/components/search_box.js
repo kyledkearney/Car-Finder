f1cb8b220895d88f86d514a944dd92de7168737e
hyper linked <45010713+kyledkearney@users.noreply.github.com>
2019-05-12 22:03:25 -0600

    styled search box

--- client/src/components/search_box.js
+++ client/src/components/search_box.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 function SearchBox() {
     return(
-    <div className='card'>
+    <div className='card text-center'>
         <br/>
+        <h1>Search for your next great read</h1>
         <form action="/search">
         <div className='form-group'>
-        <label>Search for your next great read</label>
         <input className='form-control' type="text"/>
+        </div>
+        <div className='form-group'>
+        <button className='btn btn-outline-primary btn-lg' type='submit'>Search</button>
         </div>
         </form>
     </div>
