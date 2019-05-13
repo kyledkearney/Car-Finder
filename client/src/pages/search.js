7c9978482f5931ea477828a62e2990efcf044b91
hyper linked <45010713+kyledkearney@users.noreply.github.com>
2019-05-13 16:41:52 -0600

    added .env files

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchBox from '../components/search_box'
+import Results from '../components/results'
 function Search() {
 return (
     <div className='container'>
@@ -10,6 +11,15 @@ return (
             <div className="col-lg-1"></div>
             <div className="col-lg-10">
                 <SearchBox/>
+            </div>
+            <div className="col-lg-1"></div>
+        </div>
+        <br/>
+        <br/>
+        <div className="row">
+            <div className="col-lg-1"></div>
+            <div className="col-lg-10">
+                <Results/>
             </div>
             <div className="col-lg-1"></div>
         </div>
