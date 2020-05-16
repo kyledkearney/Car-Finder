fe6d146cdfa7e95fe2030329e257007b3718d5e1
kyledkearney <kyledkearney@gmail.com>
2020-05-16 13:16:53 -0600

    search bar functioning

--- client/src/components/search_box.js
+++ client/src/components/search_box.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function SearchBox() {
+export default class SearchBox extends Component {
+    constructor(props){
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+      }
+
+      onSubmit(e) {
+        e.preventDefault();
+        var book = this.book.value;
+        console.log(book)
+    }
+
+    render() {
+
+
+
     return(
     <div className='card text-center'>
         <br/>
         <h1>Search for your next great read</h1>
         <form action="/search">
         <div className='form-group'>
-        <input className='form-control' type="text"/>
+        <input className='form-control' type="text" ref={(c) => this.book = c} name="book"/>
         </div>
         <div className='form-group'>
-        <button className='btn btn-outline-primary btn-lg' type='submit'>Search</button>
+        <button className='btn btn-outline-primary btn-lg' type='submit' onClick={this.onSubmit}>Search</button>
         </div>
         </form>
     </div>
     )
-}
-export default SearchBox;
+}}
+
